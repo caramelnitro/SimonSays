3cd6ec6e76f633c1f6bf23b470501f951bb3c0d3
Caleb <jmangrum5@my.apsu.edu>
2019-02-28 17:19:05 -0600

    Added Overrides

--- app/src/main/java/mangrum/mercer/simonsays/AboutActivity.java
+++ app/src/main/java/mangrum/mercer/simonsays/AboutActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
+    @Override
     public void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_about);
--- app/src/main/java/mangrum/mercer/simonsays/RulesActivity.java
+++ app/src/main/java/mangrum/mercer/simonsays/RulesActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class RulesActivity extends AppCompatActivity {
+    @Override
     public void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_rules);
